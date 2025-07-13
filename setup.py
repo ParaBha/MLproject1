e48b916d4b09eff37b49f73ff6c4e11ccc4327c5
Param Bhatt <parambhatt95@gmail.com>
2025-07-13 20:27:10 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'#-e . is map to setup.py
+def get_requirements(file_path:str)->List[str]:
+    '''
+    thsi function will return the list of requirements'''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        #to remove the \n from the requirements.txt
+        requirements = [req.replace("\n","")for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+
+name = 'mlproject',
+version = '0.0.1',
+author = 'Param',
+author_email = 'parambhatt95@gmail.com',
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
+
+)
+    
